f74474eb07f3af071ab722b3dd9c1b616d084d00
Johnathan Rocha <johnathanrocha@gmail.com>
2025-02-14 18:53:21 -0300

    feat: add AppTheme class for managing light and dark theme configurations

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,39 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+abstract final class AppTheme {
+  static final scheme = FlexScheme.blackWhite;
+  static ThemeData light = FlexThemeData.light(
+    scheme: scheme,
+    subThemesData: const FlexSubThemesData(
+      interactionEffects: true,
+      tintedDisabledControls: true,
+      inputDecoratorIsFilled: true,
+      inputDecoratorBorderType: FlexInputBorderType.outline,
+      alignedDropdown: true,
+      navigationRailUseIndicator: true,
+      navigationRailLabelType: NavigationRailLabelType.all,
+    ),
+    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+    cupertinoOverrideTheme: const CupertinoThemeData(applyThemeToAll: true),
+    useMaterial3: false,
+  );
+
+  static ThemeData dark = FlexThemeData.dark(
+    scheme: scheme,
+    subThemesData: const FlexSubThemesData(
+      interactionEffects: true,
+      tintedDisabledControls: true,
+      blendOnColors: true,
+      inputDecoratorIsFilled: true,
+      inputDecoratorBorderType: FlexInputBorderType.outline,
+      alignedDropdown: true,
+      navigationRailUseIndicator: true,
+      navigationRailLabelType: NavigationRailLabelType.all,
+    ),
+    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+    cupertinoOverrideTheme: const CupertinoThemeData(applyThemeToAll: true),
+    useMaterial3: false,
+  );
+}
