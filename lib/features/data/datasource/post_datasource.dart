8c5b3cd013fbb65a35650883556b790ce87a0d68
Johnathan Rocha <johnathanrocha@gmail.com>
2025-02-14 18:52:54 -0300

    feat: implement PostDatasource for fetching and creating posts using Dio

---
+++ lib/features/data/datasource/post_datasource.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:post_list_test/features/data/models/post_model.dart';
+import 'package:post_list_test/features/domain/entities/post_entity.dart';
+
+abstract interface class PostDatasource {
+  Future<List<PostEntity>> fetchPosts();
+  Future<void> createPost({required String title, required String body});
+}
+
+final class PostDatasourceImpl implements PostDatasource {
+  final Dio _dio;
+
+  PostDatasourceImpl({required Dio dio}) : _dio = dio;
+  @override
+  Future<List<PostEntity>> fetchPosts() async {
+    final Response(:data) = await _dio.get('/posts');
+
+    final List<PostEntity> posts =
+        (data as List).map((post) => PostModel.fromMap(post)).toList();
+
+    return posts;
+  }
+
+  @override
+  Future<void> createPost({required String title, required String body}) async {
+    await _dio.post('/posts', data: {'title': title, 'body': body});
+  }
+}
