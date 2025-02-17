8d675e3adb1b101e982cb74705539f0b779717a5
Johnathan Rocha <johnathanrocha@gmail.com>
2025-02-17 13:18:36 -0300

    feat: add Hive support to PostEntity with type adapter for serialization

--- lib/features/domain/entities/post_entity.dart
+++ lib/features/domain/entities/post_entity.dart
@@ -1,8 +1,15 @@
 import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
 
+part 'post_entity.g.dart';
+
+@HiveType(typeId: 0)
 class PostEntity extends Equatable {
+  @HiveField(0)
   final int id;
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final String body;
 
   const PostEntity({required this.id, required this.title, required this.body});
